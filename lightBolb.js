b7b3809f23d9951563bc219896adb82ded5d7864
Marko Kovacevic <pyrophp@gmail.com>
2013-06-08 14:10:52 +0200

    Adde validation

--- lightBolb.js
+++ lightBolb.js
@@ -9,12 +9,29 @@
 
 		var params = $.extend(defaultVal, options);
 
-		var publicMethodes = {};
+		var publicMethodes = {
+			changeCss : function(){
+				$(this).css("width",1000);
+			},
+			checkImgSize : function (orWidth, orHeight, width, height) {
+				
+				if((orWidth===width)||(orHeight===height)) {
+
+					console.log('vec namesteno');
+					return false;
+				}
+			}
+		};
 
 		return this.children().each ( function() {
 			$(this.firstElementChild).click(function(){
-				alert('marko');
-				return false;
+				var width = $(this).width();
+				var height = $(this).height();
+				var originalWidth = this.naturalWidth;
+				var originalHeight = this.naturalHeight;
+				publicMethodes.checkImgSize(originalWidth, originalHeight, width, height);
+				$(this).css({"width":originalWidth, "height":originalHeight});
+				 return false;
 			});
 		});
 	}
